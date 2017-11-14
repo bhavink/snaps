5880fe4b9b49f4e95181b6eaf4a20e4cc3cb436c
Bhavin <bhavin.z@gmail.com>
2017-11-14 14:58:57 -0500

    deleter

--- notebooks/IDP/ETL Streaming.scala
+++
@@ -1,8 +0,0 @@
-// Databricks notebook source
-val awsAccessKeyId = "AKIAIIJJOOUX67RFWLIQ"
-val awsSecretKey = "cBWysF1BStxMwcDHh0N0oSauaZW+/XDGRF5GSHnX"
-val kinesisStreamName = "IDP"
-val kinesisEndpointUrl = "https://kinesis.us-east-1.amazonaws.com"  // e.g. https://kinesis.us-west-2.amazonaws.com"
-val kinesisAppName = "IDP"
-val kinesisCheckpointIntervalSeconds = 1
-
